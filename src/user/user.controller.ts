41b89785d75a1e965c2b01ce72cb0fabd47b6a83
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2021-09-29 13:02:15 +0200

    jwt added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/createUser.dto';
-import { UserEntity } from './user.entity';
+import { UserResponseInterface } from '../types/userResponse.interface';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('user')
-  async createUser(@Body() createUserData: CreateUserDto): Promise<UserEntity> {
-    return await this.userService.createUser(createUserData);
+  async createUser(
+    @Body() createUserData: CreateUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.createUser(createUserData);
+    return this.userService.buildUserResponse(user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,9 @@ import { CreateUserDto } from './dto/createUser.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
+import { UserResponseInterface } from '../types/userResponse.interface';
+import { UserType } from '../types/user.type';
+import { sign } from 'jsonwebtoken';
 
 @Injectable()
 export class UserService {
@@ -11,10 +14,30 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
 
+  private static generateJwt(user: UserType): string {
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+    );
+  }
+
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     console.log(newUser);
     return await this.userRepository.save(newUser);
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: UserService.generateJwt(user),
+      },
+    };
   }
 }
