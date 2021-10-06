87caa9a3b6ddb4d8c3cf55be18fd7ee88d5a3cb9
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2021-10-06 12:13:34 +0200

    jwt added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,34 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserResponseInterface } from '../types/userResponse.interface';
+import { LoginUserDto } from './dto/loginUser.dto';
 
-@Controller()
+@Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('user')
+  @Post()
+  @UsePipes(new ValidationPipe())
   async createUser(
-    @Body() createUserData: CreateUserDto,
+    @Body('user') createUserDto: CreateUserDto,
   ): Promise<UserResponseInterface> {
-    const user = await this.userService.createUser(createUserData);
+    const user = await this.userService.createUser(createUserDto);
     return this.userService.buildUserResponse(user);
+  }
+
+  @Post('login')
+  @UsePipes(new ValidationPipe())
+  async loginUser(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    const loggedUser = await this.userService.loginUser(loginUserDto);
+    return this.userService.buildUserResponse(loggedUser);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -6,6 +6,8 @@ import { UserEntity } from './user.entity';
 import { UserResponseInterface } from '../types/userResponse.interface';
 import { UserType } from '../types/user.type';
 import { sign } from 'jsonwebtoken';
+import { LoginUserDto } from './dto/loginUser.dto';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -26,10 +28,53 @@ export class UserService {
   }
 
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const userByEmail = await this.userRepository.findOne({
+      email: createUserDto.email,
+    });
+
+    const userByUsername = await this.userRepository.findOne({
+      username: createUserDto.username,
+    });
+
+    if (userByEmail || userByUsername) {
+      throw new HttpException(
+        'Email or password already exists',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     console.log(newUser);
-    return await this.userRepository.save(newUser);
+    return this.userRepository.save(newUser);
+  }
+
+  async loginUser(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const userByEmail = await this.userRepository.findOne(
+      {
+        email: loginUserDto.email,
+      },
+      { select: ['id', 'email', 'username', 'password', 'image'] },
+    );
+
+    if (!userByEmail) {
+      throw new HttpException('Wrong email', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    const verifyPassword = await compare(
+      loginUserDto.password,
+      userByEmail.password,
+    );
+
+    if (!verifyPassword) {
+      throw new HttpException(
+        'Wrong password',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    delete userByEmail.password;
+    return userByEmail;
   }
 
   buildUserResponse(user: UserEntity): UserResponseInterface {
