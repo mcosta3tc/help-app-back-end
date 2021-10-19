fc4af1347b071afda9293a0348f37ea812b7cdf8
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2021-10-19 11:16:49 +0200

    posts added

--- src/database/migrations/1632902005384-CreateUsers.ts
+++ src/database/migrations/1632902005384-CreateUsers.ts
@@ -1,16 +1,28 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateUsers1632902005384 implements MigrationInterface {
-    name = 'CreateUsers1632902005384'
+  name = 'CreateUsers1632902005384';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "tags" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, CONSTRAINT "PK_e7dc17249a1148a1970748eda99" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying NOT NULL, "email" character varying NOT NULL, "image" character varying NOT NULL DEFAULT '', "password" character varying NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TABLE "tags"
+                              (
+                                  "id"   uuid              NOT NULL DEFAULT uuid_generate_v4(),
+                                  "name" character varying NOT NULL,
+                                  CONSTRAINT "PK_e7dc17249a1148a1970748eda99" PRIMARY KEY ("id")
+                              )`);
+    await queryRunner.query(`CREATE TABLE "users"
+                              (
+                                  "id"       uuid              NOT NULL DEFAULT uuid_generate_v4(),
+                                  "username" character varying NOT NULL,
+                                  "email"    character varying NOT NULL,
+                                  "image"    character varying NOT NULL DEFAULT '',
+                                  "password" character varying NOT NULL,
+                                  CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
+                              )`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "users"`);
-        await queryRunner.query(`DROP TABLE "tags"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "users"`);
+    await queryRunner.query(`DROP TABLE "tags"`);
+  }
 }
